e87bde8c990f184e6c7f77ead9aa5179add86980
vladywill <vladyslavmartyniuk13@gmail.com>
2023-10-21 15:38:36 +0100

    replace sqlite with postgres

--- db/schema.rb
+++ db/schema.rb
@@ -13,12 +13,15 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.1].define(version: 20_231_017_141_842) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension 'plpgsql'
+
   create_table 'tickets', force: :cascade do |t|
     t.text 'problem'
     t.boolean 'resolved', default: false
     t.datetime 'created_at', null: false
     t.datetime 'updated_at', null: false
-    t.integer 'user_id', null: false
+    t.bigint 'user_id', null: false
     t.index ['user_id'], name: 'index_tickets_on_user_id'
   end
 
