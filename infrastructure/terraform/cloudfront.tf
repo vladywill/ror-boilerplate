8d29babcbd9a58fac067dfde55228f65ff64e825
vladywill <vladyslavmartyniuk13@gmail.com>
2023-10-23 12:52:47 +0100

    setup terraform files, github cd & dockerfiles

---
+++ infrastructure/terraform/cloudfront.tf
@@ -0,0 +1,46 @@
+
+resource "aws_cloudfront_distribution" "app_distribution" {
+    enabled             = true
+    is_ipv6_enabled     = true
+    aliases = [var.domain]
+
+    restrictions {
+        geo_restriction {
+            restriction_type = "none"
+        }
+    }
+    price_class = "PriceClass_All"
+    viewer_certificate {
+        acm_certificate_arn = "${aws_acm_certificate_validation.cert_validation.certificate_arn}"
+        ssl_support_method = "sni-only"
+    }
+
+    origin {
+        custom_origin_config {
+            http_port = 80
+            https_port = 443
+            origin_protocol_policy = "https-only"
+            origin_ssl_protocols = ["TLSv1.2"]
+        }
+        domain_name = "${aws_lb.app_alb.dns_name}"
+        origin_id   = "app-alb"
+    }
+
+    default_cache_behavior {
+        allowed_methods  = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
+        cached_methods   = ["GET", "HEAD"]
+        target_origin_id = "app-alb"
+        viewer_protocol_policy = "redirect-to-https"
+
+        forwarded_values {
+            query_string = true
+            headers = ["*"]
+            cookies {
+                forward = "all"
+            }
+        }
+        min_ttl                = 0
+        default_ttl            = 3600
+        max_ttl                = 86400
+    }
+}
---
+++ infrastructure/terraform/load-balancer.tf
@@ -0,0 +1,65 @@
+resource "aws_lb" "app_alb" {
+  name               = "${var.environment}-app-alb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [module.alb_sg.security_group_id]
+  subnets            = module.vpc.public_subnets
+
+  enable_deletion_protection = false
+
+  tags = {
+    Environment = var.environment
+  }
+}
+
+resource "aws_lb_listener" "app_alb_listener" {
+  load_balancer_arn = aws_lb.app_alb.arn
+  port              = "443"
+  protocol          = "HTTPS"
+  certificate_arn = aws_acm_certificate_validation.alb_cert_validation.certificate_arn
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.app_alb_target.arn
+  }
+}
+
+resource "aws_lb_target_group" "app_alb_target" {
+  name     = "${var.environment}-app-target-group"
+  port     = 80
+  protocol = "HTTP"
+  target_type = "instance"
+  vpc_id   = module.vpc.vpc_id
+  lifecycle {
+      create_before_destroy = true
+  }
+  deregistration_delay = 45
+  health_check {
+    path     = "/up"
+    port     = 80
+    protocol = "HTTP"
+    healthy_threshold = 2
+    interval = 30
+    timeout = 10
+    unhealthy_threshold = 5
+  }
+}
+
+
+module "alb_sg" {
+  source  = "terraform-aws-modules/security-group/aws"
+  version = "~> 4.0"
+
+  name        = "${var.environment}-backend-alb-sg"
+  description = "ALB security group for backend. Set to allow all HTTP traffic"
+  vpc_id      = module.vpc.vpc_id
+
+  ingress_cidr_blocks = ["0.0.0.0/0"]
+  ingress_rules       = ["http-80-tcp", "https-443-tcp"]
+
+  egress_rules = ["all-all"]
+
+  tags = {
+    Environment = var.environment
+  }
+}
