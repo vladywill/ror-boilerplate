d8b43b02f873c60577a548935731c95f5df992c5
vladywill <vladyslavmartyniuk13@gmail.com>
2023-10-23 14:19:05 +0100

    tf fix default tags

--- infrastructure/terraform/main.tf
+++ infrastructure/terraform/main.tf
@@ -1,9 +1,4 @@
 terraform {
-    default_tags {
-        tags = {
-            Environment = var.environment
-        }
-    }
     required_providers {
         aws = {
             source = "hashicorp/aws"
@@ -18,6 +13,11 @@ provider "aws" {
     region = var.region
     access_key = var.aws_access_key_id
     secret_key = var.aws_secret_access_key
+    default_tags {
+        tags = {
+            Environment = var.environment
+        }
+    }
 }
 
 
