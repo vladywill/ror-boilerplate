42e110e2c565a5f2ed3208d83abd623290fde0b7
vladywill <vladyslavmartyniuk13@gmail.com>
2023-10-27 15:17:33 +0100

    add prod cable info

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -43,8 +43,8 @@ Rails.application.configure do
 
   # Mount Action Cable outside main process or domain.
   # config.action_cable.mount_path = nil
-  # config.action_cable.url = "wss://example.com/cable"
-  # config.action_cable.allowed_request_origins = [ "http://example.com", /http:\/\/example.*/ ]
+  config.action_cable.url = "wss://saas.marcusrc.com/cable
+  config.action_cable.allowed_request_origins = [/http:\/\/saas.marcusrc.com/, /https:\/\/saas.marcusrc.com/]
 
   # Assume all access to the app is happening through a SSL-terminating reverse proxy.
   # Can be used together with config.force_ssl for Strict-Transport-Security and secure cookies.
