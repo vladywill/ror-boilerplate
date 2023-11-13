8f633aa3c96df605504a7097c3e2d3dd068f4a7b
vladywill <vladyslavmartyniuk13@gmail.com>
2023-11-13 18:50:40 +0000

    update s3 config for latest aws version

--- infrastructure/terraform/storage.tf
+++ infrastructure/terraform/storage.tf
@@ -2,7 +2,15 @@ resource "aws_s3_bucket" "main_bucket" {
   bucket = "saas-marcusrc-main-bucket"
 }
 
+resource "aws_s3_bucket_ownership_controls" "main_bucket_controls" {
+  bucket = aws_s3_bucket.main_bucket.id
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
 resource "aws_s3_bucket_acl" "main_bucket_acl" {
+  depends_on = [aws_s3_bucket_ownership_controls.main_bucket_controls]
   bucket = aws_s3_bucket.main_bucket.id
   acl    = "private"
 }
